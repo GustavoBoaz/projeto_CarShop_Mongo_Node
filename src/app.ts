18fbe11bc98da4bde290f3bba9dbed4a3526dbc2
GustavoBoaz <gustavo.boaz@hotmail.com>
2022-10-26 00:31:07 -0300

    @GustavoBoaz CREATE: Middleware error event handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
 
 const app = express();
+app.use(errorHandler);
 
 export default app;
