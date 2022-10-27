62c43a0c85a56f1274be0d18c3e529dd137feea9
GustavoBoaz <gustavo.boaz@hotmail.com>
2022-10-27 00:15:50 -0300

    @GustavoBoaz RESOLVE: Task 06

--- src/tests/unit/services/seu-teste-service.test.ts
+++ src/tests/unit/services/seu-teste-service.test.ts
@@ -1,22 +1,44 @@
-// template para criação dos testes de cobertura da camada de service
+import * as sinon from 'sinon';
+import Car from '../../../models/Car';
+import CarServices from '../../../services/Car';
+import { expect }  from 'chai';
+import { carMock, carMockWithId } from '../../mocks/carsMock';
+import { ZodError } from 'zod';
 
+describe('Car Service', () => {
+  //GIVEN: Dado que tenho uma Model Car
+  const carModel = new Car();
+  //AND: E tenho serciços para Car
+  const carService = new CarServices(carModel);
+  
+  before(async () => {  
+    sinon.stub(carModel, 'create').resolves(carMockWithId);
+  });
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+  after(()=>{
+    sinon.restore();
+  });
 
-// describe('Sua descrição', () => {
+  describe('Serviço para criar um novo Car', () => {
+    it('Sucesso ao criar', async () => {
+      //WHEN: Quando utilizo o serviço de create corretamente
+      const result = await carService.create(carMock);
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+      //THEN: Então devo receber o Car criado
+      expect(result).to.be.deep.equal(carMockWithId);
+    });
 
-//   after(()=>{
-//     sinon.restore();
-//   })
-
-//   it('', async () => {});
-
-// });
+    it('Falha ao criar', async () => {
+      let result;
+      try {
+        //WHEN: Quando utilizo o serviço de create errado
+        await carService.create({});
+      } catch (error) {
+        result = error;
+      }
+      
+      //THEN: Então devo receber um Error
+      expect(result).to.be.instanceOf(Error);
+    });
+  });
+});
