876c44eed87a19617289c5c1fba58c40e211922a
GustavoBoaz <gustavo.boaz@hotmail.com>
2022-10-28 16:33:50 -0300

    @GustavoBoaz RESOLVE: Lint problem

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -2,10 +2,10 @@ import { z } from 'zod';
 import { vehicleZodSchema as VZS } from './IVehicle';
 
 const motorcycleZodSchema = VZS.extend({
-    category: z.enum(['Street', 'Custom', 'Trail']),
-    engineCapacity: z.number().int().min(1).max(2500),
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number().int().min(1).max(2500),
 });
 
 type IMotorcycle = z.infer<typeof motorcycleZodSchema>;
 
-export {motorcycleZodSchema, IMotorcycle};
+export { motorcycleZodSchema, IMotorcycle };
